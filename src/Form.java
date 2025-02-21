bb5c68dabdd53368bce41d7fc3369e5bd66ff64c
Mpoked <pokorny_marek@oauh.cz>
2025-02-21 13:14:57 +0100

    první hodina

--- src/Form.java
+++ src/Form.java
@@ -1,2 +1,25 @@
-public class Form {
+import javax.swing.*;
+
+public class Form extends JFrame {
+    private JTextField nazevDeskovky;
+    private JRadioButton hodnoceni2;
+    private JRadioButton hodnoceni3;
+    private JCheckBox vlastnímHru;
+    private JRadioButton hodnoceni1;
+    private JButton predchozi;
+    private JButton dalsi;
+    private JPanel pnMain;
+
+    public Form(){
+        this.initComponents();
+    }
+
+    public void initComponents(){
+        setContentPane(pnMain);
+        setTitle("Deskovky");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        pack();
+    }
 }
+
+
